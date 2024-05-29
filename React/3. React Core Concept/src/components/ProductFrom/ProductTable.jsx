11bbdb1d89148f518ddb38ad5cabc0806d9410d7
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-05-30 04:17:59 +0600

    docs(react): add notes on `React Core Concept`

---
+++ React/3. React Core Concept/src/components/ProductFrom/ProductTable.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ProductTable = ({ products }) => {
+  return (
+    <div>
+      <p>Total Product: {products.length}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        {products.map((product) => {
+          const { name, quantity, price } = product;
+          //   console.log(name, quantity, price);
+          return (
+            <tbody>
+              <tr>
+                <td>{name}</td>
+                <td>{quantity}</td>
+                <td>{price}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+    </div>
+  );
+};
+
+export default ProductTable;
